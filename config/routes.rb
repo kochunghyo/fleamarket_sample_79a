4ce1aadac0a67c82f4f86687680b24724aa8a963
sy151116 <y.h.913_s2@icloud.com>
2020-10-20 16:33:56 +0900

    購入内容確認マークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
   root 'items#index'
+  resources :orders, only: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
