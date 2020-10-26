82d32146d440eb66bd397f546d6859c52dac0c90
Kodai-Tsunekazu <p.b.shelley@icloud.com>
2020-10-26 20:04:05 +0900

    不要なコメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :new]
   resources :products
   root to: "items#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
