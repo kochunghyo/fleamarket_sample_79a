21236e465ddcd584556a6efc091e9daded61cdaa
kochunghyo <kc19851226@gmail.com>
2020-11-02 20:27:51 +0900

    商品出品機能サーバー再度修正①

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  # belongs_to :user
+  
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
