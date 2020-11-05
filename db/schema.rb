dbbf201d9fdfb0e24e3fc5d1e1f232756367df31
sy151116 <y.h.913_s2@icloud.com>
2020-11-05 13:31:37 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,25 @@ ActiveRecord::Schema.define(version: 2020_10_12_061940) do
     t.string "name"
     t.text "explanation"
     t.integer "price"
+<<<<<<< Updated upstream
+=======
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.date "birthday", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+>>>>>>> Stashed changes
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
