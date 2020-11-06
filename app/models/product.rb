edc0101be75633cc7712813b1cb1ea0e8ab52574
kochunghyo <kc19851226@gmail.com>
2020-11-06 16:44:25 +0900

    デプロイ修正11/6②

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   has_one :order
-  belongs_to active_hash :category
+  belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :shipping_area
