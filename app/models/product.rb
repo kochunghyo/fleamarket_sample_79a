5b156599191730bfc2aeaa158f227b14d4dec0ba
kochunghyo <kc19851226@gmail.com>
2020-11-06 16:18:34 +0900

    デプロイ修正11/6

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   has_one :order
+  belongs_to active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :shipping_area
