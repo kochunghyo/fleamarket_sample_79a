38df3eccbcc94a5807ab969571fbe0aa58ee9524
sy151116 <y.h.913_s2@icloud.com>
2020-11-09 19:11:45 +0900

    before_action追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,9 @@ class OrdersController < ApplicationController
 
   require 'payjp'
   before_action :set_card, only: [:new,:pay]
+  before_action :set_product, only: [:new,:pay]
 
   def new
-    @product = Product.find(1)
     if @card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
@@ -13,10 +13,9 @@ class OrdersController < ApplicationController
   end
 
   def pay
-    product = Product.find(1)
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => product.price, 
+    :amount => @product.price, 
     :customer => @card.customer_id, 
     :currency => 'jpy', 
   )
@@ -25,6 +24,10 @@ class OrdersController < ApplicationController
 
   def set_card
     @card = Creditcard.find_by(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(1)
   end
 
 end
